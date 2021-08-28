9e6d0b4abeccd6e85d931c40cd185d08737eb3da
Caue Santos <caue.santos@trigg.com.br>
2021-08-28 12:45:07 -0300

    Habilitando databinding

--- app/src/main/java/br/com/cauezito/simpleweatherforecast/MainActivity.kt
+++ app/src/main/java/br/com/cauezito/simpleweatherforecast/MainActivity.kt
@@ -2,10 +2,22 @@ package br.com.cauezito.simpleweatherforecast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import br.com.cauezito.simpleweatherforecast.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.btEnter.setOnClickListener{view ->
+            val zipCode = binding.etZipcode.text.toString()
+
+
+        }
     }
 }
